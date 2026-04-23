f79096c5cd2ce6be055e30497ba0418e07894bcb
victor <victor.figueroaesc@correoaiep.cl>
2026-04-23 06:10:27 -0400

    Cambio de imagenes de productos

--- js/app.js
+++ js/app.js
@@ -14,13 +14,13 @@ const productos = [
       id:1,
       nombre:"Camiseta Deportiva",
       precio:18990,
-      imagen:"https://images.unsplash.com/photo-1512436991641-6745cdb1723f"
+      imagen:"https://images.openai.com/static-rsc-4/BNtv1GukfqHqqVi6p9tS4IgaXW4kYXavRLpLJXWd8UNSC8MOKxTkGLqESVqlYqfTwyahIDpXK7LD2c6zVxafDeq05dz9OeIDJHP_WMijOIhyN-CBHeaSnSEkrqSbOTWgo_QPCTdzeoyaZMXD6G94bOQrtY-Zcqyw6heni59tU7htGOFZ4aB-XSzb0AQsSJnY?purpose=fullsize"
     },
     {
       id:2,
-      nombre:"Pantalón Deportivo",
+      nombre:"Pack gym",
       precio:12990,
-      imagen:"https://images.unsplash.com/photo-1520975916090-3105956dac38"
+      imagen:"https://images.openai.com/static-rsc-4/NWzcZeUtmJcUY4kcg4lh0BDE4U-7wd5dcw2XVxJpECtK8h1LE7EKa7ioFSWWn8vTf_asFRFdaJZmHolut2mfUz6cyXcVNLD0gUjS8L-EwukocPfW1fO222cSLUlEPa1K9OEQk2dc_VNlrydnP4LAFxzfaEasMNq76pYaX89ZTT99hYu7zxIDTIWCa_8ehnyF?purpose=fullsize"
     },
     {
       id:3,
@@ -36,15 +36,15 @@ const productos = [
     },
     {
       id:5,
-      nombre:"Barra de Pesas",
+      nombre:"Pack Crossfit",
       precio:89990,
-      imagen:"https://images.unsplash.com/photo-1571019613914-85f342c55f6c"
+      imagen:"https://images.openai.com/static-rsc-4/VSrSVbR39JgpY64rd55rGL231S7Ym4zWYVzfx2_pMpoxlNPRMbzo5qh6bgCF7-ijKT5uFbSL0ycO4NiFS0T3NI2xtxDd0HvRj9Zc8agFXmflJuRm94uTcfzmAVfPvj2TDqVB37H9PbCTy2Fao10iqtkmF-DduONuLJqYC3KKflq8tCI-AJkPADcj9d3m7UXh?purpose=fullsize"
     },
     {
       id:6,
-      nombre:"Kettlebell 8kg",
+      nombre:"Kettlebell 20kg",
       precio:19990,
-      imagen:"https://images.unsplash.com/photo-1594737625785-c43c9e3d5c63"
+      imagen:"https://images.openai.com/static-rsc-4/zHW3KVEg2vVZjWCjWJxfiM7O8V0q_o9vHBpKJab70i4DrKPpLQ6orrBBWT3nBRzrBVE-Fgza6vDZ3YgsQetdeSVlGXV4X1p6hIAcobs-a6uxtTxZtho8h5W-aEdP80Spu9kMX94hvNlknJQLo8qpksuG-e-SWCyisIcUU1YxXdIdtnJwjUDOkCSONgm4A-rw?purpose=fullsize"
     },
     {
       id:7,
@@ -54,15 +54,33 @@ const productos = [
     },
     {
       id:8,
-      nombre:"Guantes de Gimnasio",
-      precio:9990,
+      nombre:"Barra de Pesas 40kg",
+      precio:99990,
       imagen:"https://images.unsplash.com/photo-1605296867304-46d5465a13f1"
     },
     {
       id:9,
-      nombre:"Cuerda para Saltar",
+      nombre:"Ropa Deportiva",
       precio:7990,
-      imagen:"https://images.unsplash.com/photo-1599058917464-ef4d0a3c0f89"
+      imagen:"https://images.openai.com/static-rsc-4/ZKnCQI0TPxZKDjfP0lHvVEp1pV_vvjPxhpc2TL6OorPcLVSnkvTJwh0iOBeKoAOkJ9pl8fpSYQFAyQIykYr_sYKY9TOi29hfz1UDQd03sf0DSEsPSfhd8Gr2NvYBDAXvSenzrMiFAe3re6yNId8LMJf1A7EqS5aNo03qBdjDmM6UJzr92s-euE-VAg2yrKqG?purpose=fullsize"
+    },
+    {
+      id:10,
+      nombre:"Poleron mujer",
+      precio:7990,
+      imagen:"https://media.istockphoto.com/id/1441759372/photo/motivated-female-athlete-jogging-in-the-park.jpg?s=1024x1024&w=is&k=20&c=4yIFP967pnzwY4nc_A8jj1neBcHb03p7ilfbt57cBFk="
+    },
+    {
+      id:11,
+      nombre:"Alfombra de Ejercicio",
+      precio:17990,
+      imagen:"https://images.openai.com/static-rsc-4/fW_147e32jsxcyXavubm7-CMrsJsZklYC0ML8WQEHpMlVgdL8EtikvVmOpyiGiePs1nqA_DnE-IN_n-DQGFq9JNgoXzENax0u1gCg3VHD89Bx6WyAE813QQmGWugZIOHQOVdxv77ttN8KX5s1fyM8AjPXGieXA8kqxrQti0r18olacUXIwlrIlpOnBDZ5gxX?purpose=fullsize"
+    },
+    {
+      id:12,
+      nombre:"Mancuerna 15kg",
+      precio:27990,
+      imagen:"https://images.openai.com/static-rsc-4/eqWR9X-CA6Nm8P4dyi4xOdzOR3OJnWzgqt81pAy8sUbt_yDHQUUwYBE1MAjsoQ4FeHkA6MMwCmNneJbIhgI6QzERdyG_Q6_DdVaA89ybAdCHzvX33_x6I6QxPsHoWKCMNmA66cfBv6-KEcqV3gYiOpDg7lEpH2LRj3xo_iuSF90q8CBYe1zC4p6p8S25RAU3?purpose=fullsize"
     }
   ];
 
@@ -221,21 +239,54 @@ document.getElementById("checkoutForm").addEventListener("submit", function(e){
     modalPago.style.display = "flex";
   });
 
-// ================= PAGO =================
-function finalizarCompra(){
-    showToast("Compra realizada 🎉");
-
-  clearCart();
-
-  // Ocultar modal
-  document.getElementById("modalPago").style.display = "none";
-
-  // Ocultar carrito 
-  carritoBox.classList.remove("active");
-
-  // Ocultar formulario 
-  pagoSection.classList.remove("active");
-}
+  // ================= PAGO =================
+  function finalizarCompra(){
+  
+    const card = document.getElementById("cardNumber").value.trim();
+    const expiry = document.getElementById("expiry").value.trim();
+    const cvv = document.getElementById("cvv").value.trim();
+    const loading = document.getElementById("loadingPago");
+  
+    // 🔴 VALIDACIONES
+    if(!/^\d{16}$/.test(card)){
+      showToast("Tarjeta inválida (16 dígitos)");
+      return;
+    }
+  
+    if(!/^\d{2}\/\d{2}$/.test(expiry)){
+      showToast("Fecha inválida (MM/AA)");
+      return;
+    }
+  
+    if(!/^\d{3}$/.test(cvv)){
+      showToast("CVV inválido (3 dígitos)");
+      return;
+    }
+  
+    // ✅ PROCESO DE PAGO
+    loading.style.display = "block";
+  
+    setTimeout(()=>{
+  
+      loading.style.display = "none";
+  
+      showToast("Compra realizada 🎉");
+  
+      clearCart();
+  
+      // Ocultar modal
+      document.getElementById("modalPago").style.display = "none";
+  
+      // Ocultar carrito 
+      carritoBox.classList.remove("active");
+  
+      // (por si aún lo usas)
+      if(typeof pagoSection !== "undefined"){
+        pagoSection.classList.remove("active");
+      }
+  
+    },1500);
+  }
 
 // ================= IMAGEN =================
 function abrirImagen(src){
@@ -286,9 +337,9 @@ document.addEventListener("click", function(e){
 // ================= BANNER SLIDER =================
 const bannerImages = [
   "https://images.unsplash.com/photo-1517960413843-0aee8e2b3285", // pesas
-  "https://images.unsplash.com/photo-1542291026-7eec264c27ff", // zapatillas
-  "https://images.unsplash.com/photo-1521572163474-6864f9cf17ab", // camiseta
-  "https://images.unsplash.com/photo-1571019613914-85f342c55f6c", // gym
+  "https://images.unsplash.com/photo-1483721310020-03333e577078", // zapatillas
+  "https://plus.unsplash.com/premium_photo-1664537982157-136e1b911a5a", // camiseta
+  "https://plus.unsplash.com/premium_photo-1750439455715-6cca3099801f", // surf
 ];
 
 let bannerIndex = 0;
