d7b2686aa09d5443cf14715061b3662a5db8ca9a
victor <victor.figueroaesc@correoaiep.cl>
2026-04-23 21:39:54 -0400

    Agrega modal de confirmación con detalle de compra

--- js/app.js
+++ js/app.js
@@ -46,72 +46,83 @@ const productos = [
     id:1,
     nombre:"Camiseta Deportiva",
     precio:18990,
+    descripcion:"Camiseta de alta calidad, ideal para tus entrenamientos diarios. Con tecnología de absorción de sudor y diseño ergonómico para máxima comodidad.", 
     imagen:"https://images.openai.com/static-rsc-4/BNtv1GukfqHqqVi6p9tS4IgaXW4kYXavRLpLJXWd8UNSC8MOKxTkGLqESVqlYqfTwyahIDpXK7LD2c6zVxafDeq05dz9OeIDJHP_WMijOIhyN-CBHeaSnSEkrqSbOTWgo_QPCTdzeoyaZMXD6G94bOQrtY-Zcqyw6heni59tU7htGOFZ4aB-XSzb0AQsSJnY?purpose=fullsize"
   },
   {
     id:2,
     nombre:"Pack gym",
     precio:12990,
+    descripcion:"Pack completo para tu rutina de ejercicio. Incluye 5 artículos de alta calidad para maximizar tu rendimiento.",  
     imagen:"https://images.openai.com/static-rsc-4/NWzcZeUtmJcUY4kcg4lh0BDE4U-7wd5dcw2XVxJpECtK8h1LE7EKa7ioFSWWn8vTf_asFRFdaJZmHolut2mfUz6cyXcVNLD0gUjS8L-EwukocPfW1fO222cSLUlEPa1K9OEQk2dc_VNlrydnP4LAFxzfaEasMNq76pYaX89ZTT99hYu7zxIDTIWCa_8ehnyF?purpose=fullsize"
   },
   {
     id:3,
     nombre:"Zapatillas Running",
     precio:49990,
+    descripcion:"Zapatillas de running de alta calidad, diseñadas para ofrecer comodidad y soporte durante tus entrenamientos.", 
     imagen:"https://images.unsplash.com/photo-1542291026-7eec264c27ff"
   },
   {
     id:4,
     nombre:"Mancuernas 10kg",
     precio:24990,
+    descripcion:"Mancuernas de 10kg, perfectas para fortalecer tus músculos y mejorar tu rendimiento en el gimnasio.",
     imagen:"https://images.unsplash.com/photo-1583454110551-21f2fa2afe61"
   },
   {
     id:5,
     nombre:"Pack Crossfit",
     precio:89990,
+    descripcion:"Pack completo para tu rutina de Crossfit. Incluye 5 artículos de alta calidad para maximizar tu rendimiento.",
     imagen:"https://images.openai.com/static-rsc-4/VSrSVbR39JgpY64rd55rGL231S7Ym4zWYVzfx2_pMpoxlNPRMbzo5qh6bgCF7-ijKT5uFbSL0ycO4NiFS0T3NI2xtxDd0HvRj9Zc8agFXmflJuRm94uTcfzmAVfPvj2TDqVB37H9PbCTy2Fao10iqtkmF-DduONuLJqYC3KKflq8tCI-AJkPADcj9d3m7UXh?purpose=fullsize"
   },
   {
     id:6,
     nombre:"Kettlebell 20kg",
     precio:19990,
+    descripcion:"Kettlebell de 20kg, perfecta para ejercicios de fuerza y condicionamiento.",
     imagen:"https://images.openai.com/static-rsc-4/zHW3KVEg2vVZjWCjWJxfiM7O8V0q_o9vHBpKJab70i4DrKPpLQ6orrBBWT3nBRzrBVE-Fgza6vDZ3YgsQetdeSVlGXV4X1p6hIAcobs-a6uxtTxZtho8h5W-aEdP80Spu9kMX94hvNlknJQLo8qpksuG-e-SWCyisIcUU1YxXdIdtnJwjUDOkCSONgm4A-rw?purpose=fullsize"
   },
   {
     id:7,
     nombre:"Colchoneta Yoga",
     precio:14990,
+    descripcion:"Colchoneta de yoga de alta calidad, ideal para tus sesiones de estiramiento y meditación.",
     imagen:"https://images.unsplash.com/photo-1552196563-55cd4e45efb3"
   },
   {
     id:8,
     nombre:"Barra de Pesas 40kg",
-    precio:99990,
+    precio:99990, 
     imagen:"https://images.unsplash.com/photo-1605296867304-46d5465a13f1"
   },
   {
     id:9,
     nombre:"Ropa Deportiva",
     precio:7990,
+    descripcion:"Ropa de deporte de alta calidad, ideal para tus entrenamientos diarios.",
     imagen:"https://images.openai.com/static-rsc-4/ZKnCQI0TPxZKDjfP0lHvVEp1pV_vvjPxhpc2TL6OorPcLVSnkvTJwh0iOBeKoAOkJ9pl8fpSYQFAyQIykYr_sYKY9TOi29hfz1UDQd03sf0DSEsPSfhd8Gr2NvYBDAXvSenzrMiFAe3re6yNId8LMJf1A7EqS5aNo03qBdjDmM6UJzr92s-euE-VAg2yrKqG?purpose=fullsize"
   },
   {
     id:10,
     nombre:"Poleron mujer",
     precio:7990,
+    descripcion:"Poleron de alta calidad, ideal para tus entrenamientos diarios.",
     imagen:"https://media.istockphoto.com/id/1441759372/photo/motivated-female-athlete-jogging-in-the-park.jpg?s=1024x1024&w=is&k=20&c=4yIFP967pnzwY4nc_A8jj1neBcHb03p7ilfbt57cBFk="
   },
   {
     id:11,
     nombre:"Alfombra de Ejercicio",
     precio:17990,
+    descripcion:"Alfombra de ejercicio de alta calidad, ideal para tus sesiones de entrenamiento.",
     imagen:"https://images.openai.com/static-rsc-4/fW_147e32jsxcyXavubm7-CMrsJsZklYC0ML8WQEHpMlVgdL8EtikvVmOpyiGiePs1nqA_DnE-IN_n-DQGFq9JNgoXzENax0u1gCg3VHD89Bx6WyAE813QQmGWugZIOHQOVdxv77ttN8KX5s1fyM8AjPXGieXA8kqxrQti0r18olacUXIwlrIlpOnBDZ5gxX?purpose=fullsize"
   },
   {
     id:12,
     nombre:"Mancuerna 15kg",
     precio:27990,
+    descripcion:"Mancuerna de 15kg, perfecta para ejercicios de fuerza y condicionamiento.",
     imagen:"https://images.openai.com/static-rsc-4/eqWR9X-CA6Nm8P4dyi4xOdzOR3OJnWzgqt81pAy8sUbt_yDHQUUwYBE1MAjsoQ4FeHkA6MMwCmNneJbIhgI6QzERdyG_Q6_DdVaA89ybAdCHzvX33_x6I6QxPsHoWKCMNmA66cfBv6-KEcqV3gYiOpDg7lEpH2LRj3xo_iuSF90q8CBYe1zC4p6p8S25RAU3?purpose=fullsize"
   }
 ];
@@ -129,9 +140,14 @@ function renderProductos(){
   productos.forEach(p=>{
     productList.innerHTML+=`
     <div class="producto">
-      <img src="${p.imagen}" onclick="abrirImagen('${p.imagen}')">
+      <img src="${p.imagen}" onclick="abrirImagen(${p.id})">
+      
       <h3>${p.nombre}</h3>
+    
+      <p class="desc">${p.descripcion}</p> 👈 ESTA ES LA CLAVE
+    
       <p>$${p.precio}</p>
+    
       <button onclick="addToCart(${p.id})">Agregar</button>
     </div>`;
   });
